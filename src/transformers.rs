a02f5c4be6b15691145f54d091f0e69c959a3914
Ethan He <yfhe@ucdavis.edu>
2023-11-02 21:26:18 +0000

    extract the statements' tokens from the body block to avoid extra braces {}

--- src/transformers.rs
+++ src/transformers.rs
@@ -1,14 +1,14 @@
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{visit_mut::VisitMut, Expr};
+use syn::{visit_mut::VisitMut, Expr, ExprBlock};
 
 /// Convert the body of the fuzz_target! macro into a test function
-fn to_test_fn(content: Expr) -> String {
+fn to_test_fn(content_tokens: TokenStream) -> String {
     let test_fn = quote! {
         #[test]
         fn test_something() {
             let data = [];
-            #content
+            #content_tokens
         }
     };
 
@@ -27,15 +27,28 @@ impl VisitMut for ReportTransformer {
             if let Ok(parsed_macro) = syn::parse2::<syn::ExprClosure>(mac.tokens.clone()) {
                 // Transform the closure
                 let inputs = &parsed_macro.inputs;
-                let body = &parsed_macro.body;
+                let body = parsed_macro.body.as_ref().clone();
+
+                // extract the statements' tokens from the body block to avoid extra braces {}
+                let body_tokens = if let Expr::Block(ExprBlock { block, .. }) = body {
+                    block
+                        .stmts
+                        .into_iter()
+                        .map(|stmt| quote! { #stmt })
+                        .collect()
+                } else {
+                    // if it's not a block, then just use body as it is.
+                    quote! { #body }
+                };
+
                 let modified = quote! {
                     |#inputs| {
                         println!("{:?}", data);
-                        #body
+                        #body_tokens
                     }
                 };
 
-                self.test_fns.push(to_test_fn(body.as_ref().clone()));
+                self.test_fns.push(to_test_fn(body_tokens));
                 mac.tokens = TokenStream::from(modified);
             }
         }
